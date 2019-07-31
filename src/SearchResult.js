6a13bb921c503da989532c9d7f36e1771fe6e655
Ali Shirmohammadi <ali.shirmohammadi2000@gmail.com>
2019-07-31 17:20:47 +0430

    enhance search result showing

--- src/SearchResult.js
+++ src/SearchResult.js
@@ -3,9 +3,12 @@ import Paper from "@material-ui/core/Paper";
 import InputBase from "@material-ui/core/InputBase";
 import IconButton from "@material-ui/core/IconButton";
 import SearchIcon from "@material-ui/core/SvgIcon/SvgIcon";
+import {withMobileDialog} from "@material-ui/core";
 
 class SearchResult extends Component {
     constructor(props) {
+        console.log("https://www.tasnimnews.com/fa/news/1396/12/01/1661868/%D8%B3%D8%AE%D9%86%DA%AF%D9%88%DB%8C-%D8%B3%D9%BE%D8%A7%D9%87-%D8%A8%D8%A7%D9%84%DA%AF%D8%B1%D8%AF%D9%87%D8%A7%DB%8C-%D8%B3%D9%BE%D8%A7%D9%87-%D9%84%D8%A7%D8%B4%D9%87-%D9%87%D9%88%D8%A7%D9%BE%DB%8C%D9%85%D8%A7%DB%8C-%D8%AA%D9%87%D8%B1%D8%A7%D9%86-%D9%80%D9%80-%DB%8C%D8%A7%D8%B3%D9%88%D8%AC-%D8%B1%D8%A7-%D9%BE%DB%8C%D8%AF%D8%A7-%DA%A9%D8%B1%D8%AF%D9%86%D8%AF");
+        console.log(SearchResult.uriShow("https://www.tasnimnews.com/fa/news/1396/12/01/1661868/%D8%B3%D8%AE%D9%86%DA%AF%D9%88%DB%8C-%D8%B3%D9%BE%D8%A7%D9%87-%D8%A8%D8%A7%D9%84%DA%AF%D8%B1%D8%AF%D9%87%D8%A7%DB%8C-%D8%B3%D9%BE%D8%A7%D9%87-%D9%84%D8%A7%D8%B4%D9%87-%D9%87%D9%88%D8%A7%D9%BE%DB%8C%D9%85%D8%A7%DB%8C-%D8%AA%D9%87%D8%B1%D8%A7%D9%86-%D9%80%D9%80-%DB%8C%D8%A7%D8%B3%D9%88%D8%AC-%D8%B1%D8%A7-%D9%BE%DB%8C%D8%AF%D8%A7-%DA%A9%D8%B1%D8%AF%D9%86%D8%AF"));
         super(props);
         this.state = {
             query: SearchResult.parseQuery(this.props.location.search),
@@ -53,7 +56,7 @@ class SearchResult extends Component {
                             item => (
                                 <div className={"search-item"}>
                                     <a className={"search-item-title"} href={item.url}>{item.title}</a><br/>
-                                    <a className={"search-item-url"} href={item.url}>{item.url}</a><br/>
+                                    <a className={"search-item-url"} href={item.url}>{SearchResult.uriShow(item.url)}</a><br/>
                                     <span className={"search-item-text"}>{item.text}</span>
                                 </div>
                             )
@@ -66,7 +69,8 @@ class SearchResult extends Component {
     };
 
     search() {
-        fetch("http://46.4.40.237:1478/search?q=" + this.searchQuery)
+        var url = "http://46.4.40.237:1478/search?q=" + this.searchQuery;
+        fetch(url)
             .then(res => res.json())
             .then((data) => {
                 this.setState({
@@ -76,6 +80,13 @@ class SearchResult extends Component {
                 this.props.history.push("/search?q=" + this.searchQuery);
             })
             .catch(console.log)
+    }
+
+    static uriShow(uri) {
+        let decode_uri = decodeURIComponent(uri);
+        if(decode_uri.length > 100)
+            decode_uri = decode_uri.substr(0, 100) + "...";
+        return decode_uri;
     }
 
     keyDown(e) {
