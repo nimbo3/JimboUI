c0a18725e2515de1db8aeb804e3612255376b150
Ali Shirmohammadi <ali.shirmohammadi2000@gmail.com>
2019-08-17 20:51:35 +0430

    add password show and hide button

--- src/Register.js
+++ src/Register.js
@@ -4,9 +4,51 @@ import TextField from "@material-ui/core/TextField";
 import Header from "./components/header";
 import Paper from "@material-ui/core/Paper";
 import Button from "@material-ui/core/Button";
+import FormControl from "@material-ui/core/FormControl";
+import InputLabel from "@material-ui/core/InputLabel";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import IconButton from "@material-ui/core/IconButton";
+import Input from "@material-ui/core/Input";
+import Visibility from '@material-ui/icons/Visibility';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
+import InputBase from "@material-ui/core/InputBase";
+import SearchIcon from "@material-ui/core/SvgIcon/SvgIcon";
 
-export default class Register extends Component {
+
+class Register extends Component {
+    setValues(newValues) {
+        this.setState({
+            values: newValues
+        })
+    };
+
+    constructor(props: P, context: any) {
+        super(props, context);
+
+        this.state = {
+            values: {
+                amount: '',
+                password: '',
+                weight: '',
+                weightRange: '',
+                showPassword: false,
+            }
+        }
+    }
+
     render() {
+        const handleClickShowPassword = () => {
+            this.setValues({...this.state.values, showPassword: !this.state.values.showPassword});
+        };
+
+        const handleChange = (prop) => (event: React.ChangeEvent<HTMLInputElement>) => {
+            this.setValues({...this.state.values, [prop]: event.target.value});
+        };
+
+        const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
+            event.preventDefault();
+        };
+
         return (
             <div className={"App-header"}>
                 <Header/>
@@ -38,11 +80,28 @@ export default class Register extends Component {
                                 <TextField
                                     id="outlined-with-placeholder"
                                     label="Password"
+                                    type={this.state.values.showPassword ? 'text' : 'password'}
                                     placeholder="Enter password (at least 8 characters)"
                                     margin="normal"
                                     variant="outlined"
                                     className={"text-field"}
+                                    InputProps={{
+                                        endAdornment: (
+                                            <InputAdornment position="end">
+                                                <IconButton
+                                                    edge="end"
+                                                    aria-label="toggle password visibility"
+                                                    onClick={handleClickShowPassword}
+                                                    onMouseDown={handleMouseDownPassword}
+                                                >
+                                                    {this.state.values.showPassword ? <VisibilityOff /> : <Visibility />}
+                                                </IconButton>
+                                            </InputAdornment>
+                                        ),
+                                    }}
+
                                 />
+
                                 <Button variant="contained" color="primary">
                                     Register
                                 </Button>
@@ -58,3 +117,5 @@ export default class Register extends Component {
 
     }
 }
+
+export default Register;
